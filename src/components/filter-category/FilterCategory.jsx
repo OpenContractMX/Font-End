90f0ab3097bec3bac60f94d0c0dd21209e8e9a35
Leonardo Rincón <jleobotache@gmail.com>
2021-05-25 10:26:03 -0500

    I create the base structure of the filtercategory component

---
+++ src/components/filter-category/FilterCategory.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FilterCategory.scss";
+
+export const FilterCategory = () => {
+  return (
+    <div>
+      <p>soy el component filter</p>
+    </div>
+  );
+};
