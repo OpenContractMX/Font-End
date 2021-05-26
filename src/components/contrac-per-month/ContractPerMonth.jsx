ecd094401e9db1cc0726c2b6adff90dca1796abe
Leonardo Rincón <jleobotache@gmail.com>
2021-05-26 15:31:50 -0500

    I create  basic contractpermonth component and idd styles

--- src/components/contrac-per-month/ContractPerMonth.jsx
+++ src/components/contrac-per-month/ContractPerMonth.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import "./ContractPerMonth.scss";
 
 export const ContractPerMonth = () => {
-  return <div>ContractPerMonth</div>;
+  return (
+    <section className="container__contract-month">
+      <h2 className="container__contract-month--title">Contratos por Mes:</h2>
+      <div className="container__contract-month--chart">chart</div>
+    </section>
+  );
 };
